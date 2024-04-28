f98107b590efc45e8937981afb465e49e0ee8fc5
DimitrijeDenic <dimitrijesg@gmail.com>
2024-04-28 05:45:12 +0200

    removed debug

--- Assets/_Scripts/Managers/DialogueManager.cs
+++ Assets/_Scripts/Managers/DialogueManager.cs
@@ -34,7 +34,6 @@ namespace BGS.Managers
 
             foreach (var s in dialogueSo.sentences)
             {
-                print(s);
                 _sentences.Enqueue(s);
             }
             
